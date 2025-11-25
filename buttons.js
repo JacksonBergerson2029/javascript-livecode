c2d34280d1cdb9622b32477ee5df8af070960115
JacksonBergerson2029 <Bergerson.Jackson@student.greenriver.edu>
2025-11-24 18:06:33 -0800

    finished javascript livecode day 1

---
+++ buttons.js
@@ -0,0 +1,25 @@
+/*
+Block comments
+*/
+
+// Line comments
+
+// Sends a brief message to validate JS loading
+console.log("Hello!!!!!!!!");
+
+function logPress() {
+    console.log("Good hob!");
+    console.log("You clicked a button!");
+}
+
+function showAlert() {
+    alert("Hello user!");
+}
+
+// make new function toggleDark
+// log "toggleDark called"
+
+function toggleDark() {
+    console.log("toggleDark called");
+    document.body.classList.toggle("dark-mode");
+}
